a5ae4405458d6cd2fcf2a069d797165e512d5c10
yhs <mvmaniaz@gmail.com>
2020-10-18 20:21:07 +0900

    Update: webpack4 to webpack5

--- front-end/webpack.common.js
+++ front-end/webpack.common.js
@@ -16,19 +16,17 @@ module.exports = {
       {
         test: /\.(png|jpg|jpeg|gif|svg)$/,
         exclude: /node_modules/,
-        use: [
-          {
-            loader: 'url-loader',
-            options: {
-              publicPath: '',
-              name: '[name].[ext]?[hash]',
-              // file-loader로 할 경우에는 limit 속성은 없는 듯?
-              // 10KB 파일 미만의 파일은 url-loader에서 base64 인코딩 문자열로 처리
-              // 그 이상은 file-loader가 처리
-              limit: 10000 // 10KB
-            }
+        type: 'asset',
+        generator: {
+          filename: '[name][ext]?[hash]'
+        },
+        parser: {
+          dataUrlCondition: {
+            // 10KB 파일 미만의 파일은 url-loader에서 base64 인코딩 문자열로 처리
+            // 그 이상은 file-loader가 처리
+            maxSize: 10 * 1024 // 10KB
           }
-        ]
+        }
       },
       {
         test: /\.js$/,
